b2cc65dc15c620d5088557011e1499aa6521af00
Evgeniy <zhilkevich212@gmail.com>
2025-12-18 17:21:08 +0300

    create header

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,6 +1,8 @@
 import './Header.scss'
 import Logo from "@/components/Logo";
 import classNames from "classnames";
+import Button from "@/components/Button";
+import BurgerButton from "@/components/BurgerButton";
 
 const Header = (props) =>{
   const {
@@ -26,28 +28,55 @@ const Header = (props) =>{
   ]
 
   return (
-    <header className='header'>
+    <header className='header' data-js-overlay-menu=''>
       <div className="header__inner container">
         <Logo
           className={'header__logo'}
           loading={'eager'}
         />
-        <nav className="header__menu">
-          <ul className="header__menu-list">
-            {menuItems.map(({label, href}, index) =>(
-              <li className={'header__menu-item'} key={index}>
-                <a
-                  className={classNames('header__menu-link', {
-                    'is-active': href === url
-                  })}
-                  href={href}
-                >
-                  {label}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
+        <dialog
+          className="header__overlay-menu-dialog"
+          data-js-overlay-menu-dialog=''
+        >
+          <nav className="header__menu">
+            <ul className="header__menu-list">
+              {menuItems.map(({label, href}, index) =>(
+                <li className={'header__menu-item'} key={index}>
+                  <a
+                    className={classNames('header__menu-link', {
+                      'is-active': href === url
+                    })}
+                    href={href}
+                  >
+                    {label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="header__actions">
+            <Button
+              className={'header__button'}
+              label={'Search'}
+              mode={'transparent'}
+              isLabelHidden
+              iconName={'search'}
+            />
+            <Button
+              className={'header__button'}
+              label={'Search'}
+              mode={'transparent'}
+              isLabelHidden
+              iconName={'notification'}
+            />
+          </div>
+        </dialog>
+        <BurgerButton
+          className={'header__burger-button visible-tablet'}
+          extraAttrs={{
+            'data-js-overlay-menu-burger-button': ''
+          }}
+        />
       </div>
     </header>
   )
