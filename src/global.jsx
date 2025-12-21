ab99cbad950a2a51cb4e0bf429c63ca2269907cc
Evgeniy Zh. <zhilkevich212@gmail.com>
2025-12-21 23:36:34 +0300

    add hero section

--- src/global.jsx
+++ src/global.jsx
@@ -9,6 +9,7 @@ export default function (props) {
     children,
     title,
     url,
+    isHeaderFixed,
   } = props
 
   return (
@@ -46,7 +47,7 @@ export default function (props) {
           href="/site.webmanifest"
         />
       </Head>
-      <Header url={url} />
+      <Header url={url} isFixed={isHeaderFixed}/>
       <Content>
         {children}
       </Content>
