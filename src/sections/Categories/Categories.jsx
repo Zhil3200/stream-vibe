e094d63a7ba273739f48ca56ebc7ac6dc35048be
Evgeniy <zhilkevich212@gmail.com>
2025-12-22 17:05:55 +0300

    add section component

---
+++ src/sections/Categories/Categories.jsx
@@ -0,0 +1,28 @@
+import './Categories.scss'
+import classNames from 'classnames'
+import Section from "@/layouts/Section";
+
+const Categories = () => {
+
+  return (
+    <Section
+      title='Explore our wide variety of categories'
+      titleId='categories-title'
+      description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
+      actions={(
+        <div>
+          <button
+            type="button"
+          >
+            Назад
+          </button>
+        </div>
+      )}
+      isActionsHiddenOnMobile
+    >
+      CATEGORIES
+    </Section>
+  )
+}
+
+export default Categories
