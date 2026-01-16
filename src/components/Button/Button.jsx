44175234b4d13a22a77a53374d4431916d455975
Evgeniy <zhilkevich212@gmail.com>
2026-01-16 15:28:17 +0300

    add tabs component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -9,7 +9,7 @@ const Button = (props) => {
     type= 'button',
     target,
     /**
-    * '' (default) | 'transparent'| 'black-10'
+    * '' (default) | 'transparent'| 'black-10'| 'black-08'
      */
     mode = '',
     label,
